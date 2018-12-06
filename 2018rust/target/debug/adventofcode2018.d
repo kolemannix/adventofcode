b74cb52b22fc3e386c351f6120478a180ad0983b
Koleman Nix <kolemannix@gmail.com>
2018-12-06 16:30:32 -0500

    Gitignore and subdirectory

---
+++ 2018rust/target/debug/adventofcode2018.d
@@ -0,0 +1,1 @@
+/Users/kolemannix/personal/adventofcode2018/2018rust/target/debug/adventofcode2018.dSYM: /Users/kolemannix/personal/adventofcode2018/2018rust/src/day1/mod.rs /Users/kolemannix/personal/adventofcode2018/2018rust/src/day2.rs /Users/kolemannix/personal/adventofcode2018/2018rust/src/main.rs
---
+++ 2018rust/target/debug/deps/adventofcode2018-c8c78d42adb5b51f.d
@@ -0,0 +1,7 @@
+/Users/kolemannix/personal/adventofcode2018/2018rust/target/debug/deps/adventofcode2018-c8c78d42adb5b51f: src/main.rs src/day1/mod.rs src/day2.rs
+
+/Users/kolemannix/personal/adventofcode2018/2018rust/target/debug/deps/adventofcode2018-c8c78d42adb5b51f.d: src/main.rs src/day1/mod.rs src/day2.rs
+
+src/main.rs:
+src/day1/mod.rs:
+src/day2.rs:
