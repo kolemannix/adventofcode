3fdf6f0e23784e2d569938b9edc5b4ede1c21910
Koleman Nix <kolemannix@gmail.com>
2018-12-08 14:32:54 -0500

    day2 wip

--- 2018rust/src/day1/mod.rs
+++ 2018rust/src/day1/mod.rs
@@ -1,13 +1,5 @@
-use std::fs::read_to_string;
-
-fn load_file(filename: &str) -> Vec<String> {
-    let contents = read_to_string(filename).expect("something went wrong reading the file");
-    let lines = contents.lines();
-    // We need to_owned here because we need to actually put some data on the heap!
-    // Originally I was returning a Vec<&str> but my values
-    // did not have a long enough lifetime. Really cool!
-    lines.map(|s| s.to_owned()).collect::<Vec<String>>()
-}
+use std::collections::HashSet;
+use file::load_file;
 
 fn convert(num_str: &str) -> Result<i32, std::num::ParseIntError> {
     // .parse works when a leading + or - is included, e.g.,
@@ -17,16 +9,26 @@ fn convert(num_str: &str) -> Result<i32, std::num::ParseIntError> {
 
 // Use generics to accept anything that can give us an iterator
 // of anything that can be converted to a str
-fn count<I: IntoIterator<Item = T>, T: AsRef<str>>(input: I) -> i32 {
-    input
+fn repeating_balance<I: IntoIterator<Item = T>, T: AsRef<str>>(input: I) -> i32 {
+    let input_vec = input
         .into_iter()
-        .map(|x| match convert(x.as_ref()) {
-            Ok(num) => num,
-            Err(err) => {
-                eprintln!("Bad value; substituting 0. {}", err);
-                0
-            }
-        }).sum::<i32>()
+        .map(|x| convert(x.as_ref()).unwrap())
+        .collect::<Vec<i32>>();
+
+    let mut previous_frequencies: HashSet<i32> = HashSet::new();
+    let mut i = 0;
+    let mut freq = 0;
+    return loop {
+        previous_frequencies.insert(freq);
+        let cur = input_vec[i];
+        let next_freq = freq + cur;
+        if previous_frequencies.contains(&next_freq) {
+            break next_freq;
+        }
+        freq = next_freq;
+        let next_i = if i == input_vec.len() - 1 { 0 } else { i + 1 };
+        i = next_i;
+    };
 }
 
 // Accept any slice of String; this will work with vector and raw slices
@@ -44,48 +46,42 @@ fn count<I: IntoIterator<Item = T>, T: AsRef<str>>(input: I) -> i32 {
 
 pub fn solve() -> i32 {
     let problem = load_file("src/day1/input.txt");
-    let result = count(&problem);
+    let result = repeating_balance(&problem);
     //let result2 = count(&["1".to_string()]);
     println!("Frequency is {}", result);
     result
 }
 
-mod examples {
-    pub static EXAMPLE1: [&'static str; 3] = ["+1", "+1", "+1"];
-    pub static EXAMPLE2: [&'static str; 3] = ["+1", "+1", "-2"];
-    pub static EXAMPLE3: [&'static str; 3] = ["-1", "-2", "-3"];
-    pub static EXAMPLE4: [&'static str; 3] = ["1", "HELLO THERE", "1"];
-}
-
 #[cfg(test)]
 mod tests {
-    use super::count;
-    use super::examples::*;
+    use super::repeating_balance;
 
     #[test]
-    fn all_positive() {
-        let problem = ["+1", "+1", "+1"];
-        let result = count(&problem);
-        assert_eq!(result, 3);
-    }
-
-    #[test]
-    fn pos_and_neg() {
-        let problem = EXAMPLE2.to_vec();
-        let result = count(&problem);
+    fn test1() {
+        let problem = ["+1", "-1"];
+        let result = repeating_balance(&problem);
         assert_eq!(result, 0);
     }
+
     #[test]
-    fn all_neg() {
-        let problem = EXAMPLE3.to_vec();
-        let result = count(&problem);
-        assert_eq!(result, -6);
+    fn test2() {
+        let problem = ["+3", "+3", "+4", "-2", "-4"];
+        let result = repeating_balance(&problem);
+        assert_eq!(result, 10);
     }
 
     #[test]
-    fn bad_input() {
-        let problem = EXAMPLE4.to_vec();
-        let result = count(&problem);
-        assert_eq!(result, 2);
+    fn test3() {
+        let problem = ["-6", "+3", "+8", "+5", "-6"];
+        let result = repeating_balance(&problem);
+        assert_eq!(result, 5);
     }
+
+    #[test]
+    fn test4() {
+        let problem = ["+7", "+7", "-2", "-7", "-4"];
+        let result = repeating_balance(&problem);
+        assert_eq!(result, 14);
+    }
+
 }
--- 2018rust/src/main.rs
+++ 2018rust/src/main.rs
@@ -4,8 +4,8 @@ mod day2;
 fn main() {
     println!("Welcome to Advent of Rust 2018!");
 
-    let day1 = day1::solve();
-    println!("Day 1: {}", day1);
+    // let day1 = day1::solve();
+    // println!("Day 1: {}", day1);
 
     let day2 = day2::solve();
     println!("Day 2: {}", day2);
